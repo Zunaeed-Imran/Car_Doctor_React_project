f6e4211f6f95237c560f213fead3f8933c4fbdfe
imran <zunaeede@gmail.com>
2024-09-24 17:57:08 +0600

    main - setup the daisiui in the project.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+      <h1 className="text-xl font-bold underline">Hello world!</h1>
+      </div>
     </>
   );
 }
