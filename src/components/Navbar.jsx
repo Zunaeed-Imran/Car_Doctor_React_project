d5da65479103d886e0c0c70b95144fe454809802
imran <zunaeede@gmail.com>
2024-10-01 11:34:55 +0600

    main - size distance the set in the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
             <div className="w-14">
               <img alt="Tailwind CSS Navbar component" src={logoimage} />
             </div>
-            <div className="">
+            <div className="pl-20 flex gap-5">
               <Link
                 to="/"
                 className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100	 hover:bg-orange-300"
