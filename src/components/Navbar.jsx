478955eab09f774ef0e8278f3d0c285ede185a88
imran <zunaeede@gmail.com>
2024-10-01 11:27:29 +0600

    main - a bit more style on the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 // import { TiShoppingCart } from "react-icons/ti";
-// import logoimage from "../assets/logo/Logo.png";
+import logoimage from "../assets/logo/Logo.png";
 import { FaBars } from "react-icons/fa";
-import { IconContext } from "react-icons";
 import { Link } from "react-router-dom";
 
 
@@ -15,42 +14,45 @@ function Navbar() {
           {/* Hamburger menu visible on mobile */}
           <FaBars />
         </button>
-        <IconContext.Provider value={{ size: '1rem', color: '#f2f7f5' }}>
           <div className="hidden md:flex gap-3">
             {' '}
             {/* Hidden on mobile */}
-            <Link
-              to="/"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100	 hover:bg-orange-300"
-            >
-              Home
-            </Link>
-            <Link
-              to="/DoctorP"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100	 hover:bg-orange-300"
-            >
-              About
-            </Link>
-            <Link
-              to="/appointment"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
-            >
-              Services
-            </Link>
-            <Link
-              to="/patientDash"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
-            >
-              Blog
-            </Link>
-            <Link
-              to="/patientDash"
-              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
-            >
-              Contact
-            </Link>
+            <div className="w-14">
+              <img alt="Tailwind CSS Navbar component" src={logoimage} />
+            </div>
+            <div className="">
+              <Link
+                to="/"
+                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100	 hover:bg-orange-300"
+              >
+                Home
+              </Link>
+              <Link
+                to="/DoctorP"
+                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100	 hover:bg-orange-300"
+              >
+                About
+              </Link>
+              <Link
+                to="/appointment"
+                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
+              >
+                Services
+              </Link>
+              <Link
+                to="/patientDash"
+                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
+              >
+                Blog
+              </Link>
+              <Link
+                to="/patientDash"
+                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
+              >
+                Contact
+              </Link>
+            </div>
           </div>
-        </IconContext.Provider>
       </div>
     </div>
   );
