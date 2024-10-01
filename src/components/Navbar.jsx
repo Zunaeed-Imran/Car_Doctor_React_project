ef65fd36bf6170bc7ef87c678839b3b22c85093b
imran <zunaeede@gmail.com>
2024-10-01 11:40:53 +0600

    main - fix the color in the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 // import { TiShoppingCart } from "react-icons/ti";
+import { CiShoppingBasket } from "react-icons/ci";
 import logoimage from "../assets/logo/Logo.png";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -7,52 +8,60 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center fixed w-full bg-orange-50	 p-3 px-4 md:px-10 lg:px-20 z-50">
+    <div className="flex justify-between items-center fixed w-full bg-slate-50	 p-3 px-4 md:px-10 lg:px-20 z-50">
       <div className="flex items-center">
         <button className="md:hidden">
           {' '}
           {/* Hamburger menu visible on mobile */}
           <FaBars />
         </button>
-          <div className="hidden md:flex gap-3">
-            {' '}
-            {/* Hidden on mobile */}
-            <div className="w-14">
-              <img alt="Tailwind CSS Navbar component" src={logoimage} />
-            </div>
-            <div className="pl-20 flex gap-5">
-              <Link
-                to="/"
-                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100	 hover:bg-orange-300"
-              >
-                Home
-              </Link>
-              <Link
-                to="/DoctorP"
-                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100	 hover:bg-orange-300"
-              >
-                About
-              </Link>
-              <Link
-                to="/appointment"
-                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
-              >
-                Services
-              </Link>
-              <Link
-                to="/patientDash"
-                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
-              >
-                Blog
-              </Link>
-              <Link
-                to="/patientDash"
-                className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-orange-100 hover:bg-orange-300"
-              >
-                Contact
-              </Link>
-            </div>
+        <div className="hidden md:flex gap-3">
+          {' '}
+          {/* Hidden on mobile */}
+          <div className="w-14">
+            <img alt="Tailwind CSS Navbar component" src={logoimage} />
           </div>
+          <div className="pl-20 flex gap-5">
+            <Link
+              to="/"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-slate-100	 "
+            >
+              Home
+            </Link>
+            <Link
+              to="/DoctorP"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-slate-100	 "
+            >
+              About
+            </Link>
+            <Link
+              to="/appointment"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-slate-100 "
+            >
+              Services
+            </Link>
+            <Link
+              to="/patientDash"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-slate-100 "
+            >
+              Blog
+            </Link>
+            <Link
+              to="/patientDash"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-slate-100 "
+            >
+              Contact
+            </Link>
+          </div>
+          <div>
+            <Link
+              to="/patientDash"
+              className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2"
+            >
+              <CiShoppingBasket />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
