1bcab14a2674f7e8af628747bd2b285eb9322a10
imran <zunaeede@gmail.com>
2024-10-03 16:51:53 +0600

    main - styling the aboutus components.

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,21 +2,23 @@ import $aboutimg from "../assets/cardImage/aboutComponent.png";
 
 function AboutUs() {
   return (
-    <div className="p-10">
+    <div className="p-10 grid grid-cols-2">
       <div>
         <img src={$aboutimg} alt="" />
       </div>
       <div>
-        <h3 className="text-xl font-bold mb-3">This is about page</h3>
-        <h1 className="text-3xl font-bold mb-3">
+        <h3 className="text-xl text-orange-600 font-bold mb-3 text-left">
+          About Us
+        </h3>
+        <h1 className="text-3xl font-bold mb-3 text-left">
           We are qualified & of experience in this field
         </h1>
-        <p className="text-xl mb-2">
+        <p className="text-xl mb-3 text-left">
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
           humour, or randomised words which dont look even slightly believable.
         </p>
-        <p className="text-xl mb-3">
+        <p className="text-xl mb-3 text-left">
           the majority have suffered alteration in some form, by injected
           humour, or randomised words which dont look even slightly believable.
         </p>
