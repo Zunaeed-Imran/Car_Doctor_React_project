4ede443fef5a93dd847f2e5f457be242e0e7952c
imran <zunaeede@gmail.com>
2024-10-03 13:21:47 +0600

    main - adding some text content on the about component.

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,10 +7,22 @@ function AboutUs() {
         <img src={$aboutimg} alt="" />
       </div>
       <div>
-        <h1 className="text-2xl font-bold">This is about page</h1>
+        <h3 className="text-xl font-bold mb-3">This is about page</h3>
+        <h1 className="text-3xl font-bold mb-3">
+          We are qualified & of experience in this field
+        </h1>
+        <p className="text-xl mb-2">
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which dont look even slightly believable.
+        </p>
+        <p className="text-xl mb-3">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which dont look even slightly believable.
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
 export default AboutUs
