0452cc62d37de41e2214828b78d884b4688f2574
imran <zunaeede@gmail.com>
2024-10-05 11:08:34 +0600

    main - add prop type in the card.

--- src/components/card/ServiceCard.jsx
+++ src/components/card/ServiceCard.jsx
@@ -1,11 +1,26 @@
+import PropTypes from 'prop-types';
 
 
-function ServiceCard() {
+
+function ServiceCard({img, name, price}) {
   return (
     <div>
-      
+      <div className="">
+        <figure>{img}</figure>
+        <p>{name}</p>
+        <p>{price}</p>
+      </div>
     </div>
   )
 }
 
 export default ServiceCard
+
+
+
+
+ServiceCard.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+  price: PropTypes.string
+};
