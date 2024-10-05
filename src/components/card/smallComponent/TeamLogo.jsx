2993552e914d1a7e2e5fef8aed700a5542687a43
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-05 21:43:09 +0600

    main - add a logo saall card components

---
+++ src/components/card/smallComponent/TeamLogo.jsx
@@ -0,0 +1,17 @@
+import { FaFacebook, FaInstagramSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+
+
+function TeamLogo() {
+  return (
+    <div>
+      <div>
+        <FaFacebook />
+        <FaTwitterSquare />
+        <FaLinkedin />
+        <FaInstagramSquare />
+      </div>
+    </div>
+  );
+}
+
+export default TeamLogo
