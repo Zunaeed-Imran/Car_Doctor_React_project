1e21da34b9bfc0bed8987870475cc3e6ac0fccd6
imran <zunaeede@gmail.com>
2024-10-06 15:21:20 +0600

    main - make it big the logo size.

--- src/components/card/ServiceContact.jsx
+++ src/components/card/ServiceContact.jsx
@@ -4,28 +4,30 @@ import { SlCalender } from 'react-icons/sl';
 
 function ServiceContact() {
   return (
-    <div>
-      <div className="bg-black text-white">
-        <div>
+    <div className="bg-black text-white p-20 rounded-md">
+      <div className="flex flex-row gap-11 justify-between">
+        <div className=''>
           <div>
-            <SlCalender />
+            <SlCalender size={'2rem'}/>
           </div>
-          <p>We are open monday-friday</p>
-          <p>7:00 am - 9:00 pm</p>
+          <div>
+            <p className='text-xl'>We are open monday-friday</p>
+            <p className='text-2xl font-bold'>7:00 am - 9:00 pm</p>
+          </div>
         </div>
         <div>
           <div>
-            <MdAddIcCall />
+            <MdAddIcCall size={'2rem'}/>
           </div>
-          <p>Have a question?</p>
-          <p>+2546 251 2658</p>
+          <p className='text-xl'>Have a question?</p>
+          <p className='text-2xl font-bold'>+2546 251 2658</p>
         </div>
         <div>
           <div>
-            <FaLocationDot />
+            <FaLocationDot size={'2rem'}/>
           </div>
-          <p>Need a repair? our address</p>
-          <p>Liza Street, New York</p>
+          <p className='text-xl'>Need a repair? our address</p>
+          <p className='text-2xl font-bold'>Liza Street, New York</p>
         </div>
       </div>
     </div>
