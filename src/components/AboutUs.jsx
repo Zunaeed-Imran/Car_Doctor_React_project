ee1848fadb823042e11c8b64184bdf7d0242b97a
imran <zunaeede@gmail.com>
2024-10-06 14:27:12 +0600

    main - fixed styling the about us page.

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,12 +6,14 @@ function AboutUs() {
       <div>
         <img src={$aboutimg} alt="" />
       </div>
-      <div>
+      <div className="flex flex-col justify-between">
         <h3 className="text-xl text-orange-600 font-bold mb-3 text-left">
           About Us
         </h3>
-        <h1 className="text-3xl font-bold mb-3 text-left">
-          We are qualified & of experience in this field
+        <h1 className="text-5xl font-bold pb-8 text-left">
+          We are qualified <br />
+          & of experience <br />
+          in this field
         </h1>
         <p className="text-xl mb-3 text-left">
           There are many variations of passages of Lorem Ipsum available, but
@@ -22,7 +24,7 @@ function AboutUs() {
           the majority have suffered alteration in some form, by injected
           humour, or randomised words which dont look even slightly believable.
         </p>
-        <button className="btn bg-orange-600 hover:bg-orange-500 text-white">
+        <button className="btn bg-orange-600 hover:bg-orange-700 text-white w-40">
           Get More Info
         </button>
       </div>
