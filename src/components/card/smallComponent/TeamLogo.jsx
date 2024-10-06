7de23cd157c49785d67c1271f3d4fc76d3b5309c
imran <zunaeede@gmail.com>
2024-10-06 16:35:11 +0600

    main - coloring the logo and size the logo.

--- src/components/card/smallComponent/TeamLogo.jsx
+++ src/components/card/smallComponent/TeamLogo.jsx
@@ -5,10 +5,10 @@ function TeamLogo() {
   return (
     <div>
       <div className="flex">
-        <FaFacebook size={'1.5rem'}/>
-        <FaTwitterSquare size={'1.5rem'}/>
-        <FaLinkedin size={'1.5rem'}/>
-        <FaInstagramSquare size={'1.5rem'}/>
+        <FaFacebook size={'2.5rem'} color="#395185" />
+        <FaTwitterSquare size={'2.5rem'} color="#55ACEE" />
+        <FaLinkedin size={'2.5rem'} color="#0A66C2" />
+        <FaInstagramSquare size={'2.5rem'} color="#CC4587" />
       </div>
     </div>
   );
