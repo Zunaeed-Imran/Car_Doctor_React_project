91c62ee0e948f441ae299d675a6ed9ccfa78994d
imran <zunaeede@gmail.com>
2024-10-07 11:57:57 +0600

    main - finally fix the review card.

--- src/components/card/ReviewCard.jsx
+++ src/components/card/ReviewCard.jsx
@@ -4,18 +4,20 @@ function ReviewCard({ img, name, logo, title, comment, star }) {
   return (
     <div className="flex flex-col gap-2 items-start py-2 pl-10">
       <div className="flex flex-row gap-10 justify-between w-full">
-        <div>
+        <div className="pl-5">
         <img src={img} alt="" className="" />
         </div>
-        <div>
+        <div className="pr-28">
         <h2 className="text-2xl font-bold ">{name}</h2>
-        <h2 className="text-xl font-bold ">{title}</h2>
+        <h2 className="text-xl font-bold">{title}</h2>
         </div>
-        <div className="justify-end items-end">
+        <div className="justify-end items-end pr-5">
         <img src={logo} alt="quto" className="" />
         </div>
       </div>
-      {comment}
+      <div className="w-full text-left">
+        {comment}
+      </div>
       {star}
     </div>
   );
