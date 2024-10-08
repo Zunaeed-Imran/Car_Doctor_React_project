d7eec88520378286cc8ff143eb06c1b391d343a6
imran <zunaeede@gmail.com>
2024-10-08 12:51:50 +0600

    main - woking on unique car service portion.

--- src/pages/ServiceDetails.jsx
+++ src/pages/ServiceDetails.jsx
@@ -1,20 +1,35 @@
 import Navbar from "../components/Navbar"
 import Footer from "../layout/Footer"
+import serviceDetails from '../assets/pageImage/Rectangle 1539.png';
+import uniqueService from '../assets/cardImage/Rectangle 1540.png';
 
 function ServiceDetails() {
   return (
     <div>
-      <div>
-        <Navbar/>
+      <div className="pb-20">
+        <Navbar />
       </div>
-      <div>
-        
+      <div
+        className="pt-20 bg-cover bg-center h-[300px] flex items-start justify-start rounded-md"
+        style={{ backgroundImage: `url(${serviceDetails})` }}
+      >
+        <h1 className="text-3xl font-bold text-white pl-10 pt-16">
+          Service Details
+        </h1>
       </div>
-      <div>
-        <Footer/>
+      <div className="grid grid-cols-3 ">
+        <div className="grid-cols-2">
+          <img
+            src={uniqueService}
+            alt="uniqueService"
+            className="py-5" />
+        </div>
+      </div>
+      <div className="pt-20">
+        <Footer />
       </div>
     </div>
-  )
+  );
 }
 
 export default ServiceDetails
