9015e3f42c90dc3526909d9041ccf8470faca157
imran <zunaeede@gmail.com>
2024-10-08 11:39:19 +0600

    main - add footer and add padding.

--- src/pages/AddNewService.jsx
+++ src/pages/AddNewService.jsx
@@ -1,14 +1,18 @@
 import Navbar from "../components/Navbar"
+import Footer from "../layout/Footer"
 
 
 function AddNewService() {
   return (
     <div>
-      <div>
+      <div className="pb-20">
         <Navbar/>
       </div>
       <div className="pt-20">
         <h1 className="text-5xl">Add new service page.</h1>
+      </div>
+      <div className="pt-20">
+        <Footer/>
       </div>
     </div>
   )
