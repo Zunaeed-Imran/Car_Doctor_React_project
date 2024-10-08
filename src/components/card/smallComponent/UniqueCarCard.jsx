557567a2fa45b6441a3daba2fb474a7e6e6e54c5
imran <zunaeede@gmail.com>
2024-10-08 17:35:36 +0600

    main - finally style the card border color.

--- src/components/card/smallComponent/UniqueCarCard.jsx
+++ src/components/card/smallComponent/UniqueCarCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function UniqueCarCard({heading, details}) {
   return (
     <div>
-      <div className="m-2 p-5 pl-8 bg-[#F3F3F3] rounded-md">
+      <div className="m-2 p-5 pl-8 bg-[#F3F3F3] rounded-md border-t-4 border-orange-500">
         <h1 className="text-xl font-bold text-left">{heading}</h1>
         <p className="text-left leading-normal">{details}</p>
       </div>
