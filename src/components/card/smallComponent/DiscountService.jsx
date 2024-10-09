a906ec7dea2001c141dcd7026173c343a2c5a3a6
imran <zunaeede@gmail.com>
2024-10-09 14:17:40 +0600

    main - fix the padding. - fix flex of this portion

--- src/components/card/smallComponent/DiscountService.jsx
+++ src/components/card/smallComponent/DiscountService.jsx
@@ -3,8 +3,8 @@ import discount from "../../../assets/pageImage/discount.png";
 function DiscountService() {
   return (
     <div>
-      <div className="bg-black rounded-r-md justify-center items-center p-6">
-        <img src={discount} alt="discount" className=""/>
+      <div className="bg-black rounded-md flex justify-center items-center p-6 py-14">
+        <img src={discount} alt="discount" />
       </div>
     </div>
   )
