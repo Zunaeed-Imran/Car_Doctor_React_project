e4a6714fb5e81a12fc6716e59cf9a1f60c177f61
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-09 22:47:24 +0600

    main - add all the data by using props.

--- src/pages/ManageOrder.jsx
+++ src/pages/ManageOrder.jsx
@@ -4,7 +4,9 @@ import heroimg from '../assets/pageImage/Rectangle1548Shadow.png';
 import ManageOrderCard from "../components/card/smallComponent/ManageOrderCard";
 import { FaXmark } from "react-icons/fa6";
 import ApprovedButton from "../components/card/button/ApprovedButton";
-
+import img1 from "../assets/cardImage/Rectangle 65 (3).png";
+import img2 from "../assets/cardImage/dfdsfsdf.png";
+import img3 from "../assets/cardImage/Rectangle 65 (5).png";
 
 function ManageOrder() {
   return (
@@ -23,13 +25,33 @@ function ManageOrder() {
       <div>
         <ManageOrderCard
           logo={<FaXmark />}
-          img={""}
+          img={img1}
           name={"Martha Knit Top"}
           color={"Color : Green"}
           size={" Size: S"}
           mail={"info@gmail.com"}
           date={"22-10-2022"}
-          button={<ApprovedButton/>}
+          button={<ApprovedButton />}
+        />
+        <ManageOrderCard
+          logo={<FaXmark />}
+          img={img2}
+          name={"Martha Knit Top"}
+          color={"Color : Green"}
+          size={" Size: S"}
+          mail={"info@gmail.com"}
+          date={"22-10-2022"}
+          button={<ApprovedButton />}
+        />
+        <ManageOrderCard
+          logo={<FaXmark />}
+          img={img3}
+          name={"Martha Knit Top"}
+          color={"Color : Green"}
+          size={" Size: S"}
+          mail={"info@gmail.com"}
+          date={"22-10-2022"}
+          button={<ApprovedButton />}
         />
       </div>
       <div>
