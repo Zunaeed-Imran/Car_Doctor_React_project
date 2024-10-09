a394787ee910f05b8c02e825c9f813986aa9ce5f
imran <zunaeede@gmail.com>
2024-10-09 10:04:02 +0600

    main - styled card inner content.

--- src/components/card/smallComponent/SimpleStepCard.jsx
+++ src/components/card/smallComponent/SimpleStepCard.jsx
@@ -4,9 +4,9 @@ function SimpleStepCard({logo, heading, desc}) {
   return (
     <div>
       <div className=''>
-        <h1>{logo}</h1>
-        <h1>{heading}</h1>
-        <p>{desc}</p>
+        <h1 className='text-5xl text-orange-600'>{logo}</h1>
+        <h1 className='text-2xl font-bold'>{heading}</h1>
+        <p className='leading-10'>{desc}</p>
       </div>
     </div>
   )
