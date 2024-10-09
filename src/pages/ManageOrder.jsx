1e5cbd4559ff2d8017b40100f450f0a7f1dad534
imran <zunaeede@gmail.com>
2024-10-09 19:01:47 +0600

    main - done making manage order page hero section.

--- src/pages/ManageOrder.jsx
+++ src/pages/ManageOrder.jsx
@@ -1,20 +1,27 @@
 import Navbar from "../components/Navbar"
 import Footer from "../layout/Footer"
+import heroimg from '../assets/pageImage/Rectangle1548Shadow.png';
+
 
 function ManageOrder() {
   return (
     <div>
-      <div>
-        <Navbar/>
+      <div className="pb-10">
+        <Navbar />
+      </div>
+      <div
+        className="pt-20 bg-cover bg-center h-[300px] flex items-start justify-start rounded-md mb-28"
+        style={{ backgroundImage: `url(${heroimg})` }}
+      >
+        <h1 className="text-3xl font-bold text-white pl-10 pt-16">
+          Manage All Orders
+        </h1>
       </div>
       <div>
-        
-      </div>
-      <div>
-        <Footer/>
+        <Footer />
       </div>
     </div>
-  )
+  );
 }
 
 export default ManageOrder
