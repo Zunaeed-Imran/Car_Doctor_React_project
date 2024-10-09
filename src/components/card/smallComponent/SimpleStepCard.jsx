a5e4354c88a8dc068891163a8ef4ef1f24ea3472
imran <zunaeede@gmail.com>
2024-10-09 09:39:19 +0600

    main - define the prop type for the card.

---
+++ src/components/card/smallComponent/SimpleStepCard.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+function SimpleStepCard({logo, heading, desc}) {
+  return (
+    <div>
+      <div>
+        <h1>{logo}</h1>
+        <h1>{heading}</h1>
+        <p>{desc}</p>
+      </div>
+    </div>
+  )
+}
+
+export default SimpleStepCard
+
+
+SimpleStepCard.propTypes = {
+  logo: PropTypes.string,
+  heading: PropTypes.text,
+  desc: PropTypes.text,
+};
