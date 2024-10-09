08d364de9aac8e63db0c6c703b3b30837f4b7a7c
imran <zunaeede@gmail.com>
2024-10-09 17:33:37 +0600

    main - change the logo1

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -2,11 +2,11 @@ import Navbar from "../components/Navbar"
 import Footer from "../layout/Footer"
 import heroimg from "../assets/pageImage/Rectangle1548Shadow2.png";
 import CartCard from "../components/card/smallComponent/CartCard";
-import { GiSplitCross } from "react-icons/gi";
 import PendingButton from "../components/card/button/PendingButton";
 import engine from '../assets/cardImage/Rectangle 65.png';
 import autochack from '../assets/cardImage/Rectangle 65 (1).png';
 import manualchack from '../assets/cardImage/Rectangle 65 (2).png';
+import { FaXmark } from "react-icons/fa6";
 
 function CarDetails() {
   return (
@@ -24,34 +24,34 @@ function CarDetails() {
       </div>
       <div className="flex flex-col">
         <CartCard
-          logo={<GiSplitCross />}
+          logo={<FaXmark />}
           img={engine}
           name={'Martha Knit Top'}
           color={'Color : Green'}
           size={'Size: S'}
           price={'$25.00'}
           date={'22-10-2022'}
-          logo2={<PendingButton/>}
+          logo2={<PendingButton />}
         />
         <CartCard
-          logo={<GiSplitCross />}
+          logo={<FaXmark />}
           img={autochack}
           name={'Martha Knit Top'}
           color={'Color : Green'}
           size={'Size: S'}
           price={'$25.00'}
           date={'22-10-2022'}
-          logo2={<PendingButton/>}
+          logo2={<PendingButton />}
         />
         <CartCard
-          logo={<GiSplitCross />}
+          logo={<FaXmark />}
           img={manualchack}
           name={'Martha Knit Top'}
           color={'Color : Green'}
           size={'Size: S'}
           price={'$25.00'}
           date={'22-10-2022'}
-          logo2={<PendingButton/>}
+          logo2={<PendingButton />}
         />
       </div>
       <div className="pt-20">
