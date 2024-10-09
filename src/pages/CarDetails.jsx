5d307e614de9811579d11c393f44c37c0bea07db
imran <zunaeede@gmail.com>
2024-10-09 15:36:50 +0600

    main - make the hero section of cart details.

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,20 +1,26 @@
 import Navbar from "../components/Navbar"
 import Footer from "../layout/Footer"
+import heroimg from "../assets/pageImage/Rectangle1548Shadow2.png";
 
 function CarDetails() {
   return (
     <div>
-      <div>
-        <Navbar/>
+      <div className="pb-10">
+        <Navbar />
       </div>
-      <div>
-        
+      <div
+        className="pt-20 bg-cover bg-center h-[300px] flex items-start justify-start rounded-md"
+        style={{ backgroundImage: `url(${heroimg})` }}
+      >
+        <h1 className="text-3xl font-bold text-white pl-10 pt-16">
+          Cart Details
+        </h1>
       </div>
-      <div>
-        <Footer/>
+      <div className="pt-20">
+        <Footer />
       </div>
     </div>
-  )
+  );
 }
 
 export default CarDetails
