59d7af48cfd74f22d3c43db9a1f8f0ad60840a72
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-11 12:08:05 +0600

    main - all content in the right div.

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -4,33 +4,33 @@ import $heroImg from "../assets/pageImage/heroShadow.png";
 
 function Hero() {
   return (
-      <div
-        className="pt-32 bg-cover bg-center h-[300px] flex items-start justify-start rounded-md mb-28"
-        style={{ backgroundImage: `url(${$heroImg})` }}
-      >
-            <h1 className="mb-5 text-5xl font-bold text-white text-left pl-10">
-              Affordable <br /> Price For Car <br /> Servicing
-            </h1>
-            <p className="mb-5 text-white">
-              There are many variations of passages of available, but the
-              majority have suffered alteration in some form
-            </p>
-            <div className="flex gap-3">
-              <button className="btn bg-orange-600 hover:bg-orange-500 text-white">
-                Discover More
-              </button>
-              <button className="btn btn-outline hover:bg-orange-600 text-white">
-                Latest Project
-              </button>
-            </div>
-            <div>
-              <button className="btn btn-circle btn-outline bg-[#FF4500]">
-                <HiArrowSmLeft size="1.5rem" />
-              </button>
-              <button className="btn btn-circle btn-outline bg-[#FF4500]">
-                <HiArrowSmRight size="1.5rem" />
-              </button>
-            </div>
+    <div
+      className="bg-cover bg-center h-[300px] flex items-start justify-start rounded-md mb-28"
+      style={{ backgroundImage: `url(${$heroImg})` }}
+    >
+      <div>
+        <h1 className="mb-5 text-5xl font-bold text-white text-left pl-10">
+          Affordable <br /> Price For Car <br /> Servicing
+        </h1>
+        <p className="mb-5 text-white">
+          There are many variations of passages of available, but the majority
+          have suffered alteration in some form
+        </p>
+        <button className="btn bg-orange-600 hover:bg-orange-500 text-white">
+          Discover More
+        </button>
+        <button className="btn btn-outline hover:bg-orange-600 text-white">
+          Latest Project
+        </button>
+      </div>
+      <div>
+        <button className="btn btn-circle btn-outline bg-[#FF4500]">
+          <HiArrowSmLeft size="1.5rem" />
+        </button>
+        <button className="btn btn-circle btn-outline bg-[#FF4500]">
+          <HiArrowSmRight size="1.5rem" />
+        </button>
+      </div>
     </div>
   );
 }
