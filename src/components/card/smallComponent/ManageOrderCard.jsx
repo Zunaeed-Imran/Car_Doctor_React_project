63cf8117f1076183b3608cf2570118a1f51106de
imran <zunaeede@gmail.com>
2024-10-14 17:04:01 +0600

    main - done task - make manage all order apge responsive. - all the page is responsive now in this project.

--- src/components/card/smallComponent/ManageOrderCard.jsx
+++ src/components/card/smallComponent/ManageOrderCard.jsx
@@ -4,19 +4,19 @@ import PropTypes from 'prop-types';
 
 function ManageOrderCard({logo, img, name, color, size, mail, date, button}) {
   return (
-    <div className='flex flex-row justify-between px-20 items-center'>
-      <p className='text-2xl bg-black rounded-full text-white p-3'>{logo}</p>
+    <div className="grid md:grid-cols-3 md:pb-5 lg:grid-cols-6 justify-between lg:px-20 items-center gap-4">
+      <p className="text-2xl bg-black rounded-full text-white h-10 w-10 p-3 flex items-center">{logo}</p>
       <img src={img} alt="carEngine" />
-      <div className='text-left'>
-        <p className='text-xl font-bold'>{name}</p>
+      <div className="text-left">
+        <p className="text-xl font-bold">{name}</p>
         <p>{color}</p>
         <p>{size}</p>
       </div>
-      <p className='font-bold'>{mail}</p>
-      <p className='font-bold'>{date}</p>
+      <p className="font-bold">{mail}</p>
+      <p className="font-bold">{date}</p>
       <p>{button}</p>
     </div>
-  )
+  );
 }
 
 export default ManageOrderCard
