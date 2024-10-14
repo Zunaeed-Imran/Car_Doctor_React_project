9af64444658bfc60a3ce7a9fe0ee8522e4d1a3b9
imran <zunaeede@gmail.com>
2024-10-14 13:03:33 +0600

    main - done task: popular product section responsive.

--- src/components/PopularProduct.jsx
+++ src/components/PopularProduct.jsx
@@ -21,7 +21,7 @@ function PopularProduct() {
           believable.
         </p>
       </div>
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <PopularProductCard
           img={carEngine}
           star={<RatingProduct />}
