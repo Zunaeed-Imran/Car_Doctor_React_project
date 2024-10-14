2cab31c87353b503d61c3c78b7d341bf804c3547
imran <zunaeede@gmail.com>
2024-10-14 17:05:39 +0600

    main - add padding bottom when the page is midium.

--- src/components/card/smallComponent/ManageOrderCard.jsx
+++ src/components/card/smallComponent/ManageOrderCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 function ManageOrderCard({logo, img, name, color, size, mail, date, button}) {
   return (
-    <div className="grid md:grid-cols-3 md:pb-5 lg:grid-cols-6 justify-between lg:px-20 items-center gap-4">
+    <div className="grid md:grid-cols-3 md:pb-5 lg:grid-cols-6 justify-between lg:px-20 items-center gap-4 pb-5">
       <p className="text-2xl bg-black rounded-full text-white h-10 w-10 p-3 flex items-center">{logo}</p>
       <img src={img} alt="carEngine" />
       <div className="text-left">
