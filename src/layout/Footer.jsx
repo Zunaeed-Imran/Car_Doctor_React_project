62c625d90a0060e4ed152b36d639e145518577e3
imran <zunaeede@gmail.com>
2024-10-14 10:44:19 +0600

    main - styled the heading text of the footer.

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,27 +6,27 @@ function Footer() {
     <div>
       <footer className="footer bg-black text-white p-10">
         <aside>
-          <img src={logo} alt="" className='pb-5'/>
+          <img src={logo} alt="" className="pb-5" />
           <p className="text-left leading-relaxed pb-5">
             Edwin Diaz is a software and web <br />
             technologies engineer, a life coach <br />
             trainer who is also a serial .
           </p>
-          <FooterLogo/>
+          <FooterLogo />
         </aside>
         <nav>
-          <h6 className="footer-title">About</h6>
+          <h6 className="text-xl font-bold text-white pb-6">About</h6>
           <a className="link link-hover">Home</a>
           <a className="link link-hover">Service</a>
           <a className="link link-hover">Contact</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Company</h6>
+          <h6 className="text-xl font-bold text-white pb-6">Company</h6>
           <a className="link link-hover">Why Car Doctor</a>
           <a className="link link-hover">About</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Support</h6>
+          <h6 className="text-xl font-bold text-white pb-6">Support</h6>
           <a className="link link-hover">Support Center</a>
           <a className="link link-hover">Feedback</a>
           <a className="link link-hover">Accesbility</a>
