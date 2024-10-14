0fa7e2acb4c4f3960a54213dcb8be2ceda9bbf7e
imran <zunaeede@gmail.com>
2024-10-14 10:46:25 +0600

    main - done task. - styled the footer section.

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -15,18 +15,18 @@ function Footer() {
           <FooterLogo />
         </aside>
         <nav>
-          <h6 className="text-xl font-bold text-white pb-6">About</h6>
+          <h6 className="py-10 text-xl font-bold text-white pb-6">About</h6>
           <a className="link link-hover">Home</a>
           <a className="link link-hover">Service</a>
           <a className="link link-hover">Contact</a>
         </nav>
         <nav>
-          <h6 className="text-xl font-bold text-white pb-6">Company</h6>
+          <h6 className="py-10 text-xl font-bold text-white pb-6">Company</h6>
           <a className="link link-hover">Why Car Doctor</a>
           <a className="link link-hover">About</a>
         </nav>
         <nav>
-          <h6 className="text-xl font-bold text-white pb-6">Support</h6>
+          <h6 className="py-10 text-xl font-bold text-white pb-6">Support</h6>
           <a className="link link-hover">Support Center</a>
           <a className="link link-hover">Feedback</a>
           <a className="link link-hover">Accesbility</a>
