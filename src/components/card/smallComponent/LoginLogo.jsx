07b9e9706b8da1fb7af6aa65928c3013aa23b003
imran <zunaeede@gmail.com>
2024-10-14 11:17:44 +0600

    main - change the bg-color of the logo.

--- src/components/card/smallComponent/LoginLogo.jsx
+++ src/components/card/smallComponent/LoginLogo.jsx
@@ -4,10 +4,16 @@ import { FaFacebookF, FaGoogle, FaLinkedinIn } from "react-icons/fa";
 function LoginLogo() {
   return (
     <div>
-      <div className="flex gap-4 justify-center pt-4">
-        <FaFacebookF />
-        <FaLinkedinIn />
-        <FaGoogle />
+      <div className="flex gap-4 justify-center pt-4 ">
+        <div className="bg-[#F5F5F8] p-3 rounded-full">
+          <FaFacebookF size={'1.5rem'} color="#3B5998" />
+        </div>
+        <div className="bg-[#F5F5F8] p-3 rounded-full">
+          <FaLinkedinIn size={'1.5rem'} color="#0A66C2" />
+        </div>
+        <div className="bg-[#F5F5F8] p-3 rounded-full">
+          <FaGoogle size={'1.5rem'} color="#EB4132" />
+        </div>
       </div>
     </div>
   );
