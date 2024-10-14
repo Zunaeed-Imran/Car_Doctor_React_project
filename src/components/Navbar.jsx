7e95a915afcc21aac36a870997d82675ce99d860
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-14 21:57:03 +0600

    main - add scroll to about page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@
 import { CiSearch, CiShoppingBasket } from "react-icons/ci";
 import logoimage from "../assets/logo/Logo.png";
 import { FaBars } from "react-icons/fa";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 import AppointmentButton from "./card/button/AppointmentButton";
 
 
@@ -12,12 +13,12 @@ function Navbar() {
     <div className="flex top-0 justify-between items-center fixed w-full bg-slate-50	 p-3 px-4 md:px-10 lg:px-20 z-50">
       <div className="flex items-center">
         <button className="md:hidden">
-          {' '}
+          {" "}
           {/* Hamburger menu visible on mobile */}
           <FaBars />
         </button>
         <div className="hidden md:flex gap-3">
-          {' '}
+          {" "}
           {/* Hidden on mobile */}
           <div className="w-14">
             <img alt="Tailwind CSS Navbar component" src={logoimage} />
@@ -30,7 +31,9 @@ function Navbar() {
               Home
             </Link>
             <Link
-              to="/test"
+              to="aboutus"
+              smooth={true}
+              duration={500}
               className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2 bg-slate-100	 "
             >
               About
@@ -65,13 +68,13 @@ function Navbar() {
               to="/cardetails"
               className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2"
             >
-              <CiShoppingBasket size={'1.5rem'} />
+              <CiShoppingBasket size={"1.5rem"} />
             </Link>
             <Link
               to="/manageorder"
               className="btn glass flex border-white border rounded-lg px-4 py-2 items-center gap-2"
             >
-              <CiSearch size={'1.5rem'} />
+              <CiSearch size={"1.5rem"} />
             </Link>
             <Link to="/checkout">
               <AppointmentButton />
