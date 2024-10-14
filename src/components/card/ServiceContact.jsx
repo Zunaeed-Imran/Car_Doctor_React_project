88fec56ed07e58a0ddebe186e11760afc9e4268e
imran <zunaeede@gmail.com>
2024-10-14 12:53:56 +0600

    main - done task - service contact card make responsive.

--- src/components/card/ServiceContact.jsx
+++ src/components/card/ServiceContact.jsx
@@ -5,21 +5,21 @@ import { SlCalender } from 'react-icons/sl';
 function ServiceContact() {
   return (
     <div className="bg-black text-white p-20 rounded-md">
-      <div className="grid grid-cols-1 md:grid-cols-3">
-          <div>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+          <div className='text-left'>
             <p className="text-xl flex items-center gap-3">
               <SlCalender size={'2rem'} /> We are open monday-friday
             </p>
             <p className="text-2xl font-bold">7:00 am - 9:00 pm</p>
         </div>
-        <div>
+        <div className='text-left'>
           <p className="text-xl flex items-center gap-3">
             <MdAddIcCall size={'2rem'} />
             Have a question?
           </p>
           <p className="text-2xl font-bold">+2546 251 2658</p>
         </div>
-        <div>
+        <div className='text-left'>
           <p className="text-xl flex items-center gap-3">
             <FaLocationDot size={'2rem'} />
             Need a repair? our address
