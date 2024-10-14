a680d64050d811d1c5b31e61abc7d9fadcf0facf
imran <zunaeede@gmail.com>
2024-10-14 16:32:37 +0600

    main - button orange color change.

--- src/components/card/button/PendingButton.jsx
+++ src/components/card/button/PendingButton.jsx
@@ -2,7 +2,7 @@
 
 function PendingButton() {
   return (
-    <div className="btn text-white bg-orange-600">
+    <div className="btn text-white bg-[#ff3811] hover:bg-red-600">
       <button>Pending</button>
     </div>
   );
