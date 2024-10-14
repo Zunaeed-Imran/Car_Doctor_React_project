67655d0751e0f1945e3be75e13a5374705040594
imran <zunaeede@gmail.com>
2024-10-14 16:48:27 +0600

    main - trying to responsive the cart card page.

--- src/components/card/smallComponent/CartCard.jsx
+++ src/components/card/smallComponent/CartCard.jsx
@@ -2,8 +2,11 @@ import PropTypes from 'prop-types';
 
 function CartCard({logo, img, name, color, size, price, date, logo2}) {
   return (
-    <div className="grid md:grid-cols-4 lg:grid-cols-6 justify-between lg:px-20 items-center">
-      <p className="text-2xl text-white bg-black rounded-full p-3">{logo}</p>
+    <div className="grid md:grid-cols-4 md:pb-5 lg:grid-cols-6 justify-between lg:px-20 items-center gap-4">
+      <p className="text-2xl text-white bg-black rounded-full h-10 w-10 flex items-center justify-center">
+        {logo}
+      </p>
+
       <img src={img} alt="product" />
       <div className="flex flex-col gap-2 pt-3">
         <p className="font-bold text-xl">{name}</p>
