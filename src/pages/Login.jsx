5c2b52857ba294a8495e53112ede62a45a003422
imran <zunaeede@gmail.com>
2024-10-21 09:56:45 +0600

    main - trying to responsive the login page.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
           <img src={loginimg} alt="" />
         </div>
         <div className="pt-32 items-center justify-center">
-          <div className="border-2 border-[#D0D0D0] rounded-md p-10 px-20">
+          <div className="md:border-2 border-[#D0D0D0] rounded-md md:p-10 md:px-20">
             <form action="" className="flex flex-col gap-4">
               <h1 className="text-3xl font-bold py-7">Login</h1>
               <label htmlFor="" className="font-semibold m-1 text-left">
